9cab4a831c091b3ac648246f8a31db33b0908c5a
Harsh <145785080+harsh-sreehari@users.noreply.github.com>
2024-09-19 22:15:15 +0530

    Update hand_tracker.py

--- hand_tracker.py
+++ hand_tracker.py
@@ -5,26 +5,41 @@ import mediapipe as mp
 cap = cv.VideoCapture(0)
 
 mphands = mp.solutions.hands
-hands = mphands.Hands()
+hands = mphands.Hands(min_detection_confidence=0.4,min_tracking_confidence=0.4)
 mp_draw = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
 
 
 while True:
     success, img = cap.read()
-    imgRGB = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    if not success:
+        continue
+    img = cv.flip(img, 1)
+
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    
     
 
-    results = hands.process(imgRGB)
-    cv.imshow('Hand Tracker', img)
+    results = hands.process(img)
+
+    
     
     if results.multi_hand_landmarks:
+        print("landmarks detected")
         for hand_landmarks in results.multi_hand_landmarks:
-            mp_draw.draw_landmarks(img, hand_landmarks)
+            mp_draw.draw_landmarks(img,
+                                   hand_landmarks,
+                                   mphands.HAND_CONNECTIONS,
+                                   mp_drawing_styles.get_default_hand_landmarks_style(),
+                                   mp_drawing_styles.get_default_hand_connections_style()
+                                    )
 
+    cv.imshow('Hand Tracker', img)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
+cap.release()
 cv.destroyAllWindows()
     
